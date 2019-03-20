07410296a332baee8bdd28d801123fa38d4d9193
Jun Tian <juti@microsoft.com>
2019-03-20 18:41:46 +0800

    expose variables

--- gen/LibTemplate.jl
+++ gen/LibTemplate.jl
@@ -22,10 +22,25 @@ include(joinpath(@__DIR__, "..", "gen", "libhanabi_common.jl"))
 include(joinpath(@__DIR__, "..", "gen", "libhanabi_api.jl"))
 
 # export everything
-#foreach(names(@__MODULE__, all=true)) do s
-#    if startswith(string(s), "SOME_PREFIX")
-#        @eval export $s
-#    end
-#end
+
+function camel_to_underscore(s)
+    replace(s, r"[A-Z]" => x -> "_" * lowercase(x))[2:end]
+end
+
+foreach(names(@__MODULE__, all=true)) do s
+    if startswith(string(s), "Py")
+        s_type = Symbol(string(s)[3:end])
+        @eval begin
+            const $s_type = $s
+            export $s_type
+        end
+    elseif match(r"([A-Z][a-z]*)+", string(s)) !== nothing
+        s_func = Symbol(camel_to_underscore(string(s)))
+        @eval begin
+            const $s_func = $s
+            export $s_func
+        end
+    end
+end
 
 end # module
