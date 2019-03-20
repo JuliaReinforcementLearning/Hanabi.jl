d96db399f57bda136f02c0e9089d16cc4ce881d8
TianJun <tianjun@wisetechai.com>
2019-03-20 17:50:42 +0800

    fix wrap error

--- gen/generator.jl
+++ gen/generator.jl
@@ -5,7 +5,9 @@ const HANABI_H = joinpath(@__DIR__, "..", "deps", "usr", "include", "pyhanabi.h"
 wc = init(; headers = [HANABI_H],
             output_file = joinpath(@__DIR__, "libhanabi_api.jl"),
             common_file = joinpath(@__DIR__, "libhanabi_common.jl"),
+	    clang_includes = [CLANG_INCLUDE],
             header_wrapped = (root, current) -> root == current,
+	    header_library = x->"libhanabi",
             clang_diagnostics = true)
 
 run(wc)
