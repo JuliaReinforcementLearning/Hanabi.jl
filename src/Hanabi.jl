d94e2c7fd4645ecca4254c9a2297d4b6aa18d673
Jun Tian <juti@microsoft.com>
2019-03-21 18:10:59 +0800

    update

--- src/Hanabi.jl
+++ src/Hanabi.jl
@@ -1,18 +1,8 @@
 module Hanabi
 
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("Hanabi was not build properly. Please run Pkg.build(\"Hanabi\").")
-end
+include(joinpath(@__DIR__, "..", "gen", "LibTemplate.jl"))
+@reexport using .LibHanabi
 
-include(depsjl_path)
-
-include(joinpath(@__DIR__, "..", "gen", "libhanabi_common.jl"))
-include(joinpath(@__DIR__, "..", "gen", "libhanabi_api.jl"))
-
-# Module initialization function
-function __init__()
-    check_deps()
-end
+include("utils.jl")
 
 end # module
